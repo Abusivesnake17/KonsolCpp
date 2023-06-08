2fe7fa99ca643d53cd3599b4f786cfa355eaa241
Abusivesnake <103052898+Abusivesnake@users.noreply.github.com>
2023-06-08 10:58:49 +0300

    Create KonsolaSayıYazdırma.cpp
    Konsola girilen yazıyı yazdıran bir kod eklendi.

---
+++ KonsolaSayıYazdırma.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int sayi;
+    printf("Bir sayi giriniz: ");
+    scanf("%d", &sayi);
+    printf("Girdiginiz sayi degeri: %d", sayi);
+    return 0;
+}
