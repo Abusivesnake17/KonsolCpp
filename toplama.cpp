98c80c117567c7783e56545512ecf8e4f560a3f7
Yiğithan Fırat <103052898+Abusivesnake@users.noreply.github.com>
2024-05-01 16:24:51 +0300

    Addition Process
    Addition process codes have been added.

---
+++ toplama.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1, n2, sum;
+    cout << "Enter the first number: ";
+    cin >> n1;
+    cout << "Enter the second number: ";
+    cin >> n2;
+    sum = n1 + n2;
+    cout << "Addition is: " << sum;
+}
