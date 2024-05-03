ea0c4a5562b66e8513669c4ae97f11796ea50ce6
Abusivesnake <yigithanfirat@gmail.com>
2024-05-03 13:59:01 +0300

    launch.json update
    Update to launch.json

---
+++ cikarma.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1, n2, sub;
+    cout << "Enter the first number: ";
+    cin >> n1;
+    cout << "Enter the second number: ";
+    cin >> n2;
+    sub = n1 - n2;
+    cout << "Subtraction is: " << sub;
+}
